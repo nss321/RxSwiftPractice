63b1333087f5b875012262d88fcc2aa973065234
BAE <lexon1209@gmail.com>
2025-02-19 00:37:23 +0900

    feat: BirthDayView 구현

--- RxSwiftPractice/Presentation/BirthDayViewController.swift
+++ RxSwiftPractice/Presentation/BirthDayViewController.swift
@@ -12,4 +12,45 @@ import SnapKit
 
 final class BirthDayViewController: ViewController {
     
+    private let datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.datePickerMode = .date
+        picker.preferredDatePickerStyle = .wheels
+        return picker
+    }()
+    
+    private let selectedDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "날짜를 선택해주세요."
+        label.textColor = .label
+        return label
+    }()
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y년 M월 d일"
+        return formatter
+    }()
+    
+    override func bind() {
+        datePicker.rx.date
+            .map { [weak self] date in
+                self?.dateFormatter.string(from: date) ?? ""
+            }
+            .bind(to: selectedDateLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    override func configView() {
+        [datePicker, selectedDateLabel].forEach { view.addSubview($0) }
+        
+        datePicker.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        selectedDateLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(datePicker.snp.bottom).offset(16)
+        }
+    }
 }
